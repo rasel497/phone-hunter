401ec82d9a67d1ab7e94c613da0dc6d37927af3a
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-08-31 10:27:11 +0600

    console api data show

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('Phone Hunter!');
+const loadphones = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=phone`
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+
+}
+
+loadphones();
