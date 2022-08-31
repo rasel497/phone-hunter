57780d8870b70e88765ebe5b38010f87d6467ee6
Rashel Hossain <55279456+rasel497@users.noreply.github.com>
2022-08-31 10:35:54 +0600

    Add files via upload
    display result by createElement innerHTML  show phones

--- js/app.js
+++ js/app.js
@@ -2,8 +2,27 @@ const loadphones = async () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=phone`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    displayPhones(data.data);
 
+}
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phones-container');
+    phoneContainer.textContent = ''; // specific phone by  search
+    // using slice show 20 phones in display
+    phones = phones.slice(0, 5);
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+            <div class="card p-4">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text"> ${phone.slug}</p>
+            </div>
+            `;
+        phoneContainer.appendChild(phoneDiv);
+    })
 }
 
 loadphones();
